7739fc94e53caeae29848ce1f7f765fd82be2218
Tjaitil <52608380+Tjaitil@users.noreply.github.com>
2025-07-31 21:07:10 +0200

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +13,9 @@ export default defineConfig({
     globals: true,
   },
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
